40df25b4e082c303015669e6cf32b3ffd7ec630a
moonhy7 <moonhy7@naver.com>
2021-09-13 16:21:49 +0900

    21.9.13.Mon

---
+++ Scripts/notice실습.sql
@@ -0,0 +1,24 @@
+INSERT INTO `lecture`.`notice`(
+	`TITLE`, -- 백팁(``)쓰는 이유 : 이름 안에 띄어쓰기가 있는 경우 때문에 
+	`WRITER_ID`,
+	`CONTENT`,
+	`HIT`)
+VALUES(
+	'[가입인사]반갑습니다',
+    'moonhy7',
+    '반갑습니다',
+    '100'
+);
+
+
+INSERT INTO `lecture`.`notice`(
+	`TITLE`,
+	`WRITER_ID`,
+	`CONTENT`,
+	`HIT`)
+VALUES(
+	'[あいさつ]',
+    'ひとみ',
+    'お会いできて嬉しいです',
+    '7'
+);
